6a6f6e4384f6309345590a4c9a8bc21bac9c9803
arminbw <arminbw@users.noreply.github.com>
2025-10-22 00:13:26 +0200

    testing interrupt draw routine

--- includes/gameCode.asm
+++ includes/gameCode.asm
@@ -4,10 +4,20 @@ main:
 
 
 gameSetup:
+  sei
+  lda #%00110110 // Disable BASIC
+  sta $0001
+  cli
+
+  // lda #%00011110 // Screen RAM: $0400   Charset: $3800
+  // sta $d018
+
+  // lda #%11011000 // Enable screen multicolor
+  // sta SCREEN_CONTROL_2
 	jsr INTERRUPT.setupRasterInterrupt
   jsr INTERRUPT.enableRasterInterrupt
-
   // DrawChar(2,5,7,1)
     
 gameLoop:
+  // DrawChar(2,5,7,1)
   jmp gameLoop
